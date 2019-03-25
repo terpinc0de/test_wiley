5455d5aa2081bb5124b190ca99224f97fdbc9183
Dmitry Donskoy <dmidev@yandex.ru>
2019-03-26 02:24:20 +0500

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,52 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {TodoModule} from './todo/todo.module';
+import {TodoComponent} from './todo/todo.component';
+import {TodoDataSourcing} from './datasource/todo.datasourcing';
+import {TodoLocalStorageDataSource} from './datasource/todo-local-storage.datasource';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatFormFieldModule, MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule
+} from '@angular/material';
+import { FormComponent } from './todo/form/form.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {TodoStorage} from './storages/todo.storage';
+import { ListComponent } from './todo/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    FormComponent,
+    ListComponent,
   ],
   imports: [
     BrowserModule,
-    TodoModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [
+    TodoStorage,
+    {
+      provide: TodoDataSourcing,
+      useClass: TodoLocalStorageDataSource,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
